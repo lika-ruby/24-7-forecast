398e4ce455439992a6e8b227a392e203a6a26835
lika-ruby <mary.havrylova.2011@gmail.com>
2025-12-12 21:46:40 +0200

    makes all

--- src/fetch/gets/getLocation.jsx
+++ src/fetch/gets/getLocation.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_OPENWEATHER_KEY;
+
 export const getLocation = async (city, limit) => {
   const response = await axios.get(
-    "http://api.openweathermap.org/geo/1.0/direct",
+    "https://api.openweathermap.org/geo/1.0/direct",
     {
       params: {
         q: city,
         limit,
-        appid: "366259874904f9faa4c4a5c3c3a6deab",
+        appid: API_KEY,
       },
     }
   );
